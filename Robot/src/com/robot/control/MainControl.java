4991a4788e40fd9850e4f7c8e4b824ec0b865a42
schulzh <schulzh@in.tum.de>
2013-07-11 15:58:06 +0200

    Menu working

--- Robot/src/com/robot/control/MainControl.java
+++ Robot/src/com/robot/control/MainControl.java
@@ -1,7 +1,5 @@
 package com.robot.control;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.IntentFilter;
 import android.os.Bundle;
@@ -9,12 +7,14 @@ import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainControl extends Activity {
+public class MainControl extends FragmentActivity {
 
 	BluetoothBroadcastReceiver bcr = null;
 	private final IntentFilter intentFilter = new IntentFilter();
 	public boolean btConnected = false;
+	ControlUnits cu = new ControlUnits();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +24,26 @@ public class MainControl extends Activity {
 		intentFilter.addAction(android.bluetooth.BluetoothDevice.ACTION_ACL_CONNECTED);
 		intentFilter.addAction(android.bluetooth.BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
 
-		setContentView(R.layout.activity_main_control);
-		// setContentView(R.layout.fragment_control_unit);
+		setContentView(R.layout.welcomescreen);
+
+		
+		
 
 	}
 
+	
+	public void openController(View v) {
+		setContentView(R.layout.activity_main_control);
+		if(findViewById(R.id.mainFragment)!=null) {
+			
+			getSupportFragmentManager().beginTransaction().add(R.id.mainFragment, cu).commit();
+		}
+
+
+		
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
@@ -38,10 +53,8 @@ public class MainControl extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		final ControlUnits cu = (ControlUnits) getFragmentManager().findFragmentById(R.id.fragment1);
 		switch (item.getItemId()) {
 		case R.id.atn_connect:
-			
 			cu.connectBT(btConnected);
 			return true;
 		case R.id.atn_gyro:
@@ -67,4 +80,6 @@ public class MainControl extends Activity {
 		super.onDestroy();
 	}
 
+	
+	
 }
