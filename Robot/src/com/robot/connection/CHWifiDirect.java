c3eb2a45957f9fb7ebe40c636f02cea3e80fe08c
schulzh <schulzh@in.tum.de>
2013-07-14 12:54:28 +0200

    added wifi direct stub

---
+++ Robot/src/com/robot/connection/CHWifiDirect.java
@@ -0,0 +1,25 @@
+package com.robot.connection;
+
+import java.io.IOException;
+
+public class CHWifiDirect implements ConnectionHandlerInterface {
+
+	@Override
+	public void sendData(String s) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeConnection() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void establishConnection() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
