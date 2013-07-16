9cd8cad97de2fbd5c512e158b3f0329d87703b84
fischers <fischers@in.tum.de>
2013-07-16 18:24:24 +0200

    Added some function stubs

--- Robot/src/com/robot/ai/Navigator.java
+++ Robot/src/com/robot/ai/Navigator.java
@@ -1,5 +1,66 @@
 package com.robot.ai;
 
-public class Navigator {
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+
+public class Navigator implements SensorEventListener {
+
+	private SensorManager sensorManager;
+	private Sensor sensorAccelerometer;
+	private Sensor sensorMagneticField;
+	private Activity mActivity;
+
+	private float[] valuesAccelerometer;
+	private float[] valuesMagneticField;
+
+	private float[] matrixR;
+	private float[] matrixI;
+	private float[] matrixValues;
+
+	public Navigator(Activity act) {
+
+		mActivity = act;
+
+	}
+
+	public void init() {
+
+		sensorManager = (SensorManager) mActivity
+				.getSystemService(Context.SENSOR_SERVICE);
+		sensorAccelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorMagneticField = sensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+		valuesAccelerometer = new float[3];
+		valuesMagneticField = new float[3];
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void getPosition(){
+		//TODO
+	}
+	
+	public float getBearing(Location dest){
+		//TODO
+		return 0f;
+	}
+	
 
 }
