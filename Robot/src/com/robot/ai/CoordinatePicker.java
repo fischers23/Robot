888e2916986c058f9c25d20718954c7058e5539a
schulzh <schulzh@in.tum.de>
2013-07-17 19:41:48 +0200

    mylocation added

--- Robot/src/com/robot/ai/CoordinatePicker.java
+++ Robot/src/com/robot/ai/CoordinatePicker.java
@@ -2,6 +2,7 @@ package com.robot.ai;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -16,28 +18,43 @@ import com.robot.R;
 
 public class CoordinatePicker extends Fragment {
 	private View v;
+	private MapView m;
 	private GoogleMap map;
-	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-
-	// mapView
-	MapView m;
+	LatLng ENGLISCHER_GARTEN = new LatLng(48.1298926770173, 11.583151817321777);
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		// inflat and return the layout
+
+		// inflate and return the layout
 		View v = inflater.inflate(R.layout.fragment_map, container, false);
 		m = (MapView) v.findViewById(R.id.mapView);
 		m.onCreate(savedInstanceState);
 
 		map = m.getMap();
-		
-		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-		        .title("Hamburg"));
-		
 
-	    
+		// LocationManager lm = (LocationManager)
+		// getActivity().getSystemService(Context.LOCATION_SERVICE);
+		// Criteria criteria = new Criteria();
+		// criteria.setAccuracy(Criteria.ACCURACY_LOW);
+		// String bbb = lm.getBestProvider(criteria, true);
+		// Location myLocation = lm.getLastKnownLocation(bbb);
+		// CameraUpdate camPos = CameraUpdateFactory.newLatLng(new
+		// LatLng(myLocation.getLatitude(), myLocation.getLongitude()));
+
+		if (map != null) {
+//			map.addMarker(new MarkerOptions().position(ENGLISCHER_GARTEN).title("Englischer Garten"));
+//			CameraPosition mCameraPosition = new CameraPosition.Builder().target(ENGLISCHER_GARTEN).bearing(0).tilt(30).build();
+//			map.animateCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+			map.setMyLocationEnabled(true);
+//			map.setOnMapClickListener(listener)
+		} else
+			Log.e("CoordinatePicker", "map is null");
+
 		return v;
 	}
+	
+	
+	
 
 	@Override
 	public void onResume() {
