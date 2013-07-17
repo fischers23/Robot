ae347aded5a63b016ec8a4a031b450c084012197
schulzh <schulzh@in.tum.de>
2013-07-17 18:46:31 +0200

    Fucking map funktioniert!!!! Additional: added an idea for a popup to explain the different mode of our app.

--- Robot/src/com/robot/ai/CoordinatePicker.java
+++ Robot/src/com/robot/ai/CoordinatePicker.java
@@ -8,48 +8,59 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.robot.R;
 
 public class CoordinatePicker extends Fragment {
+	private View v;
+	private GoogleMap map;
+	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 
-	
-	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	  static final LatLng KIEL = new LatLng(53.551, 9.993);
-	  private GoogleMap map;
+	// mapView
+	MapView m;
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		  super.onCreate(savedInstanceState);
-			View mContentView = inflater.inflate(R.layout.fragment_map, container, false);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// inflat and return the layout
+		View v = inflater.inflate(R.layout.fragment_map, container, false);
+		m = (MapView) v.findViewById(R.id.mapView);
+		m.onCreate(savedInstanceState);
+
+		map = m.getMap();
+		
+		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+		        .title("Hamburg"));
+		
 
 	    
-	    
-//	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-//	        .getMap();
-//	    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-//	        .title("Hamburg"));
-//	    Marker kiel = map.addMarker(new MarkerOptions()
-//	        .position(KIEL)
-//	        .title("Kiel")
-//	        .snippet("Kiel is cool")
-//	        .icon(BitmapDescriptorFactory
-//	            .fromResource(R.drawable.ic_launcher)));
-//
-//	    // Move the camera instantly to hamburg with a zoom of 15.
-//	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-//
-//	    // Zoom in, animating the camera.
-//	    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-	    
-	    
-	    
-	    return mContentView;
-	  }
-	  
-	    
+		return v;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		m.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		m.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		m.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		m.onLowMemory();
+	}
+
 }
--- Robot/src/com/robot/ui/AIDriver.java
+++ Robot/src/com/robot/ui/AIDriver.java
@@ -13,68 +13,69 @@ import com.robot.ai.CoordinatePicker;
 import com.robot.ai.Navigator;
 import com.robot.connection.ArduinoCommands;
 
-public class AIDriver extends Fragment  {
+public class AIDriver extends Fragment {
 
 	Navigator navi;
 
-	
 	// the arduino command set
 	ArduinoCommands driver = null;
-	
 
 	View mContentView;
 
-	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	
 
 		mContentView = inflater.inflate(R.layout.fragment_ai, container, false);
-		
-		
-		navi  = new Navigator(getActivity());
+
+		navi = new Navigator(getActivity());
 		navi.init();
-		
+
 		Button btn = (Button) mContentView.findViewById(R.id.refresh);
 		btn.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				refresh();
-				
+
 			}
 		});
-		
+
 		Button mapOpen = (Button) mContentView.findViewById(R.id.map_open);
 		mapOpen.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				// initialize the AI fragment
+				// // initialize the AI fragment
 				CoordinatePicker copi = new CoordinatePicker();
-				
+
 				getFragmentManager().beginTransaction().replace(R.id.mainFragment, copi, "copi").addToBackStack("copi").commit();
-		
+
+				// mContentView.inflate(getActivity(), R.layout.fragment_map,
+				// null);
+
+				// getActivity().setContentView(R.layout.fragment_map);
+				// SupportMapFragment mapFrag = (SupportMapFragment)
+				// getFragmentManager().findFragmentById(R.id.fragment_map);
+				// map = mapFrag.getMap();
+
 			}
 		});
-		
+
 		return mContentView;
 	}
-	
-	public void refresh(){
+
+	public void refresh() {
 		TextView tv = (TextView) mContentView.findViewById(R.id.azimuth);
-		tv.setText(""+navi.getAzimuth());
+		tv.setText("" + navi.getAzimuth());
 		tv = (TextView) mContentView.findViewById(R.id.pitch);
-		tv.setText(""+navi.getPitch());
+		tv.setText("" + navi.getPitch());
 		tv = (TextView) mContentView.findViewById(R.id.roll);
-		tv.setText(""+navi.getRoll());
+		tv.setText("" + navi.getRoll());
 	}
-	
-	
+
 	public void setCommands(ArduinoCommands ac) {
 		driver = ac;
 	}
-	
-	
+
 }
