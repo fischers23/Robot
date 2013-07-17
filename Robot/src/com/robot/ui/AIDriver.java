b714cfdaa228ffe69c919e7acabd395b7d0d37cf
fischers <fischers@in.tum.de>
2013-07-17 14:37:25 +0200

    Added some test methods for compass

--- Robot/src/com/robot/ui/AIDriver.java
+++ Robot/src/com/robot/ui/AIDriver.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.robot.R;
 import com.robot.ai.Navigator;
@@ -12,21 +14,35 @@ import com.robot.ai.Navigator;
 public class AIDriver extends Fragment  {
 
 	Navigator navi;
-	
+	View mContentView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		View mContentView = inflater.inflate(R.layout.fragment_ai, container, false);
+		mContentView = inflater.inflate(R.layout.fragment_ai, container, false);
 		
 		
 		navi  = new Navigator(getActivity());
+		navi.init();
 		
-		
+		Button btn = (Button) mContentView.findViewById(R.id.refresh);
+		btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				refresh();
+				
+			}
+		});
 		
 		return mContentView;
 
 		
+	}
+	
+	public void refresh(){
+		TextView tv = (TextView) mContentView.findViewById(R.id.azimuth);
+		tv.setText(""+navi.getAzimuth());
 	}
 	
 	
