0d270b5c957aa229d9e55a4cf7e65ed91deff069
schulzh <schulzh@in.tum.de>
2013-07-20 11:13:09 +0200

    compass bubble

--- Robot/src/com/robot/ai/CoordinatePicker.java
+++ Robot/src/com/robot/ai/CoordinatePicker.java
@@ -1,5 +1,9 @@
 package com.robot.ai;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -7,13 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.robot.R;
 
 public class CoordinatePicker extends Fragment {
@@ -30,31 +35,49 @@ public class CoordinatePicker extends Fragment {
 		m = (MapView) v.findViewById(R.id.mapView);
 		m.onCreate(savedInstanceState);
 
+		// get the map object
 		map = m.getMap();
-
-		// LocationManager lm = (LocationManager)
-		// getActivity().getSystemService(Context.LOCATION_SERVICE);
-		// Criteria criteria = new Criteria();
-		// criteria.setAccuracy(Criteria.ACCURACY_LOW);
-		// String bbb = lm.getBestProvider(criteria, true);
-		// Location myLocation = lm.getLastKnownLocation(bbb);
-		// CameraUpdate camPos = CameraUpdateFactory.newLatLng(new
-		// LatLng(myLocation.getLatitude(), myLocation.getLongitude()));
-
 		if (map != null) {
-//			map.addMarker(new MarkerOptions().position(ENGLISCHER_GARTEN).title("Englischer Garten"));
-//			CameraPosition mCameraPosition = new CameraPosition.Builder().target(ENGLISCHER_GARTEN).bearing(0).tilt(30).build();
-//			map.animateCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+			
+			// get current position
+			LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+			Criteria criteria = new Criteria();
+			criteria.setAccuracy(Criteria.ACCURACY_LOW);
+			String bbb = lm.getBestProvider(criteria, true);
+			Location myLocation = lm.getLastKnownLocation(bbb);
+			LatLng location = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+			Log.d("CoordinatePicker", location.toString());
+
+			try {
+				// initialize map interaction
+				MapsInitializer.initialize(getActivity());
+			} catch (GooglePlayServicesNotAvailableException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			// go to current position
+			CameraPosition mCameraPosition = new CameraPosition.Builder().target(location).bearing(0).tilt(30).zoom(15f).build();
+			map.animateCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+			
+			// make button to current location available
 			map.setMyLocationEnabled(true);
-//			map.setOnMapClickListener(listener)
+			
+			// set onclick listener to grab a position
+			map.setOnMapClickListener(new OnMapClickListener() {
+				@Override
+				public void onMapClick(LatLng point) {
+					// get click point
+					Log.d("CoordinatePicker", point.toString());
+
+				}
+			});
+
 		} else
 			Log.e("CoordinatePicker", "map is null");
-
 		return v;
 	}
-	
-	
-	
 
 	@Override
 	public void onResume() {
